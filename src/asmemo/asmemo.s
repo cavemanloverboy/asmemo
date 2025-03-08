f29704d0475d560fc4685a923f39875562967ef2
cavey cool <caveycool@gmail.com>
2025-03-08 16:45:46 +0000

    nonzero account support: add signer check, nundup check

--- src/asmemo/asmemo.s
+++ src/asmemo/asmemo.s
@@ -1,3 +1,7 @@
+
+
+# [num_accounts u64][[account info (88 bytes)]][account data][rent_epoch] .. [next account] .. [ix data len] [ix data] [program_id]
+
 .globl entrypoint
 entrypoint:
 
@@ -12,27 +16,57 @@ entrypoint:
     # r2 = num_accounts (u64)
     # r3-r9 = 0
     # Load num accounts
-    ldxdw r2, [r1 + 0]                                              // 1 CU
-    jne r2, 0, fail_nonzero_accounts                                // 2 CU
+    ldxdw r2, [r1 + 0]
+        
 
-    # After this operation
-    # r0 = 0
-    # r1 = ptr to start of ix data
-    # r2 = len of ix data
-    # r3-r9 = 0
-    # Load ix data length (this assumes num_accounts is zero)
-    ldxdw r2, [r1 + 8]                                              // 3 CU 
-    add64 r1, 16 // skip over num_accounts (8), ix data length (8)  // 4 CU
+    # Now, loop through accounts and check signer byte is set
+    account_loop:
+        jeq r2, 0, log_message
+
+        # Load duplicate byte into r3 and check for nondup
+        ldxb r3, [r1 + 8]
+        jne r3, 255, duplicate_account_error
+        
+        # If nondup, load signer byte into r3 and check its nonzero
+        ldxb r3, [r1 + 9]
+        call sol_log_64_
+        jeq r3, 0, nonsigner_error
+
+        # Now load account data length into r3
+        # round up to nearest multiple of 8
+        # and increment cursor by this much (and account info and rent epoch and padding)
+        ldxdw r3, [r1 + 88]
+        add64 r3, 7
+        and64 r3, 0xFFFFFFFFFFFFFFF8
+        add64 r1, 88 + 10240 + 8 // account info, padding, rent epoch
+        add64 r1, r3 // data
+
+        sub64 r2, 1 // Decrement account counter
+        ja account_loop
+    
+    log_message:
+        # After this operation
+        # r0 = 0
+        # r1 = ptr to start of ix data
+        # r2 = len of ix data
+        # r3-r9 = 0
+        # Load ix data length (this assumes num_accounts is zero)
+        ldxdw r2, [r1 + 8]
+        add64 r1, 16 // skip over num_accounts (8), ix data length (8)
 
 
     # TODO: validate ascii
-    call sol_log_                                                   // 105 CU
+    call sol_log_
 
-    exit                                                            // 106 CU
+    exit
 
+    duplicate_account_error:
+        lddw r0, 420
+        exit
 
-    fail_nonzero_accounts:
-        exit                                                        // 3 CU
+    nonsigner_error:
+        lddw r0, 69
+        exit
 
 .rodata
     message: .ascii "Hello, Solana!"
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 #[cfg(test)]
 mod tests {
     use litesvm::LiteSVM;
-    use solana_sdk::{instruction::Instruction, message::Message, native_token::LAMPORTS_PER_SOL, pubkey::Pubkey, signature::Keypair, signer::Signer, transaction::Transaction};
+    use solana_sdk::{account::Account, instruction::{AccountMeta, Instruction, InstructionError}, message::Message, native_token::LAMPORTS_PER_SOL, pubkey::Pubkey, signature::Keypair, signer::Signer, transaction::{Transaction, TransactionError}};
 
 
     const ID: Pubkey = solana_sdk::pubkey!("asmemoasmemoasmemoasmemoasmemoasmemoasmemoa");
 
     #[test]
-    fn test_valid_ascii() { 
+    fn test_valid_ascii_no_accounts() { 
         let mut svm = LiteSVM::new().with_sigverify(false).with_blockhash_check(false);
         svm.add_program_from_file(ID, "deploy/asmemo.so").unwrap();
         let keypair = Keypair::new();
@@ -32,4 +32,97 @@ mod tests {
             println!("    {log}");
         }
     }
+
+    #[test]
+    fn test_valid_ascii_two_accounts() { 
+        let mut svm = LiteSVM::new().with_sigverify(false).with_blockhash_check(false);
+        svm.add_program_from_file(ID, "deploy/asmemo.so").unwrap();
+        let keypair = Keypair::new();
+        let payer = keypair.pubkey();
+        svm.airdrop(&payer, 100 *LAMPORTS_PER_SOL).unwrap();
+
+        // Initialize second account with data
+        let first_account = payer;
+        let second_account = Pubkey::new_unique();
+        svm.set_account(
+            second_account,
+            Account::new_data(LAMPORTS_PER_SOL, &[42_u8; 17], &Pubkey::default()).unwrap()).unwrap();
+
+        const MEMO: &str = "why does spl memo use 36000 cus to print len 60 msg of ascii";
+        let instruction = Instruction::new_with_bytes(
+            ID,
+            MEMO.as_bytes(),
+            vec![AccountMeta::new_readonly(first_account, true), AccountMeta::new_readonly(second_account, true)]
+        );
+
+        let message = Message::new(&[instruction], Some(&payer));
+        let transaction = Transaction::new_unsigned(message);
+        
+        let result = svm.send_transaction(
+            transaction,
+        ).unwrap();
+        for log in result.logs {
+            println!("    {log}");
+        }
+    }
+
+    #[test]
+    fn test_fail_nonsigner() { 
+        let mut svm = LiteSVM::new().with_sigverify(false).with_blockhash_check(false);
+        svm.add_program_from_file(ID, "deploy/asmemo.so").unwrap();
+        let keypair = Keypair::new();
+        let payer = keypair.pubkey();
+        svm.airdrop(&payer, 100 *LAMPORTS_PER_SOL).unwrap();
+
+        // Initialize second account with data
+        let second_account = Pubkey::new_unique();
+        const MEMO: &str = "why does spl memo use 36000 cus to print len 60 msg of ascii";
+        let instruction = Instruction::new_with_bytes(
+            ID,
+            MEMO.as_bytes(),
+            vec![AccountMeta::new_readonly(second_account, false)]
+        );
+
+        let message = Message::new(&[instruction], Some(&payer));
+        let transaction = Transaction::new_unsigned(message);
+        
+        let result = svm.send_transaction(
+            transaction,
+        ).unwrap_err();
+    
+        assert!(matches!(result.err, TransactionError::InstructionError(0, InstructionError::Custom(69))));
+      
+    }
+
+    // TODO fail duplicate account
+    #[test]
+    fn test_fail_duplicate() { 
+        let mut svm = LiteSVM::new().with_sigverify(false).with_blockhash_check(false);
+        svm.add_program_from_file(ID, "deploy/asmemo.so").unwrap();
+        let keypair = Keypair::new();
+        let payer = keypair.pubkey();
+        svm.airdrop(&payer, 100 *LAMPORTS_PER_SOL).unwrap();
+
+        // Initialize second account with data
+        let second_account = Pubkey::new_unique();
+        const MEMO: &str = "why does spl memo use 36000 cus to print len 60 msg of ascii";
+        let instruction = Instruction::new_with_bytes(
+            ID,
+            MEMO.as_bytes(),
+            vec![AccountMeta::new_readonly(second_account, true); 2]
+        );
+
+        let message = Message::new(&[instruction], Some(&payer));
+        let transaction = Transaction::new_unsigned(message);
+        
+        let result = svm.send_transaction(
+            transaction,
+        ).unwrap_err();
+    
+        assert!(matches!(result.err, TransactionError::InstructionError(0, InstructionError::Custom(420))));
+      
+    }
 }
+
+
+
